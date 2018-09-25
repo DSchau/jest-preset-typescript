56087968f3d548773a9778f716f90f4c2f4065b2
Dustin Schau <dustinschau@gmail.com>
2018-09-25 09:12:42 -0500

    chore: fix publish

--- scripts/build.js
+++ scripts/build.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const { name } = require(path.resolve('package.json'));
 
-const preset = require(path.resolve(`dist/${name}.js`));
+const preset = require(path.resolve(`${name}.js`));
 
-fs.writeFileSync(path.resolve('dist/jest-preset.json'), JSON.stringify(preset, null, 2) + '\n', 'utf8');
+fs.writeFileSync(path.resolve('jest-preset.json'), JSON.stringify(preset, null, 2) + '\n', 'utf8');
